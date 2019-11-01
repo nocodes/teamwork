9ee7063269ad99f1814e883b92ae466e74c799a2
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-11-01 01:16:56 +0100

    feature(api-endpoint): add version to api endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
-app.use('/api/', routers);
+app.use('/api/v1/', routers);
 
 app.use((request, response) => {
   response.status(404).send({
