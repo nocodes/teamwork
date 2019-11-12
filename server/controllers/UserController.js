237b2872e18c5b34ab80b3b30d671bec6fdb5bb3
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-11-12 19:08:11 +0100

    feature(employee signin): create signin controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -20,7 +20,21 @@ class UserController {
       'userId' : saveUser.rows[0].id
     });
   }
+
+  static async signIn(request, response) {
+    const { email, password } = request.body;
+    const _user = await user.getByEmail(email);
+    Helpers.dbError(response, _user);
+    if (_user.count > 0 && Helpers.comparePassword(_user.row.password, password)) {
+      const token = Helpers.generateToken(_user.row.id);
+      return Helpers.sendResponse(response, 200,   { 
+        'token' : token,
+        'userId' : _user.row.id
+      });
+    }
+    return Helpers.sendFailedResponse(response, 400, 'Invalid credentials');
+  } 
   
 }
 
-export default UserController; 
+export default UserController;  
