2ae84ed10faac6312122c8ddea7fd4396c840deb
akandeBolaji <akandbolajidanielofficial@gmail.com>
2019-11-18 09:40:41 +0100

    feature(single gif): create findone method

--- server/routers/gifs.js
+++ server/routers/gifs.js
@@ -9,7 +9,7 @@ gifs.use(validation);
 // Gifs APIs
 gifs.route('/gifs').post(GifsController.store); //add cloudinary
 gifs.route('/gifs/:gifId').delete(GifsController.destroy);
-gifs.route('/gifs/:gifId').get();
+gifs.route('/gifs/:gifId').get(GifsController.findOne);
 gifs.route('/gifs/:gifId/comments').post(GifsController.addComment);
 
 export default gifs; 
